6efc07da3f7d407bfe0716b414cda1b4bfad01c7
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-12-09 12:49:13 +0500

    bug fixed

---
+++ Functions/Logger.js
@@ -0,0 +1,10 @@
+const Logger=(type,message,info)=>{
+console.log("\n==================================================================")
+console.log(` ${type.toString().toUpperCase()} ----> ${message}`)
+info && console.log(` INFO    ----> ${info}`)
+console.log("==================================================================\n")
+}
+
+Logger('success','/get/id','golden')
+
+module.exports = {Logger}
