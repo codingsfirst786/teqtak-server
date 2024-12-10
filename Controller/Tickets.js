409b935859ec88a6434ef1c42779fdc6d3dc92d7
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-12-10 14:37:38 +0500

    bug fixed

--- Controller/Tickets.js
+++ Controller/Tickets.js
@@ -4,7 +4,7 @@ const User = require('../Schemas/User')
 const jwt = require("jsonwebtoken")
 const { Mail_Factroy } = require("../Mail/Mail_Factory")
 const { v4: uuidv4 } = require('uuid');
-const {Logger} = require('../Functions/Logger')
+const { Logger } = require('../Functions/Logger')
 const { get } = require('../Mail/transport')
 
 
@@ -59,15 +59,15 @@ const getEventTickets = async (req, res) => {
         const poster = await User.get(event.eventCreatedBy)
         // console.log({poster})    
         const ticket = await Ticket.scan('ticketEventId').eq(id).exec()
-        let data = await Promise.all(ticket.map(async(e)=>{
+        let data = await Promise.all(ticket.map(async (e) => {
             try {
                 const buyer = await User.get(e.ticketBuyerId)
-                return {...e,buyer}
+                return { ...e, buyer }
             } catch (error) {
                 return null
             }
         }))
-        data = data.filter((e)=>e!=null)
+        data = data.filter((e) => e != null)
         res.json({
             count: data.length,
             poster,
@@ -82,6 +82,24 @@ const getEventTickets = async (req, res) => {
     }
 
 }
+
+const getALLEventsTickets = async (req, res) => {
+    try {
+
+        let allEventIds = await Ticket.scan().attributes(['ticketEventId']).exec()
+        allEventIds = allEventIds.map((e) => e.ticketEventId)
+
+        const data = await TicketMapper(allEventIds)
+        res.json(data)
+
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+
+    }
+
+}
+
 const createTicket = async (req, res) => {
     try {
         const _id = uuidv4()
@@ -102,55 +120,55 @@ const createTicket = async (req, res) => {
 
 const getATicket = async (req, res) => {
     try {
-        
-    const ticket = await Ticket.get(req.params.id)
-    const event_ = await Event.get(ticket.ticketEventId)
-    const buyer_ = await User.get(ticket.ticketBuyerId)
-    const buyer = buyer_ || {}
-    const event = event_ || {}
-    const seller_ = await User.get(event.eventCreatedBy)
-    const seller = seller_ || {}
 
-    res.json({...ticket,event,seller,buyer})
-
-} catch (error) {
-        console.log({error})
-        res.send("error ",error)
-}
-}
-
-const getAllData=async(ticket)=>{
-    const data = await Promise.all(ticket.map(async(e,i)=>{
-        try{
-        const event_ = await Event.get(e.ticketEventId)
-        const buyer_ = await User.get(e.ticketBuyerId)
+        const ticket = await Ticket.get(req.params.id)
+        const event_ = await Event.get(ticket.ticketEventId)
+        const buyer_ = await User.get(ticket.ticketBuyerId)
         const buyer = buyer_ || {}
         const event = event_ || {}
         const seller_ = await User.get(event.eventCreatedBy)
         const seller = seller_ || {}
-        return {
-            ...e,
-            buyer,
-            event,
-            seller
+
+        res.json({ ...ticket, event, seller, buyer })
+
+    } catch (error) {
+        console.log({ error })
+        res.send("error ", error)
+    }
+}
+
+const getAllData = async (ticket) => {
+    const data = await Promise.all(ticket.map(async (e, i) => {
+        try {
+            const event_ = await Event.get(e.ticketEventId)
+            const buyer_ = await User.get(e.ticketBuyerId)
+            const buyer = buyer_ || {}
+            const event = event_ || {}
+            const seller_ = await User.get(event.eventCreatedBy)
+            const seller = seller_ || {}
+            return {
+                ...e,
+                buyer,
+                event,
+                seller
+            }
         }
-    }
-    catch(e){
-        console.log({e})
-        return null
-    }
+        catch (e) {
+            console.log({ e })
+            return null
+        }
 
     }))
-    const filtered = data.filter((e)=>e!=null)
+    const filtered = data.filter((e) => e != null)
     return filtered
 
 }
 
-const ticketPostAndriod = async(req,res)=>{
+const ticketPostAndriod = async (req, res) => {
     try {
         console.log("posting through andriod")
         const _id = uuidv4()
-        console.log("body is ",req.body.data)
+        console.log("body is ", req.body.data)
         console.log("body is ", process.env.JWT_AND)
         const metadata = jwt.verify(req.body.data, process.env.JWT_AND)
         const ticket = new Ticket({ _id, ...metadata })
@@ -161,11 +179,44 @@ const ticketPostAndriod = async(req,res)=>{
 
     } catch (error) {
         console.log(error)
-        res.send("error",error)
+        res.send("error", error)
 
     }
 
 
 }
 
-module.exports = { getAllTickets, getMyTickets, getEventTickets, createTicket,getATicket,ticketPostAndriod }
+
+
+async function TicketMapper(eventArray) {
+
+    let bigData = await Promise.all(eventArray.map(async (e,i) => {
+        try{
+        const event = await Event.get(e,{ attributes: ["_id", "eventTitle",'eventCreatedBy'] })
+        const poster = await User.get(event.eventCreatedBy,{ attributes: ["name", "email","Users_PK"] })
+        const ticket = await Ticket.scan('ticketEventId').eq(e).attributes(['ticketBuyerId','totalAmount']).exec()
+        let data = await Promise.all(ticket.map(async (e) => {
+           
+                const buyer = await User.get(e.ticketBuyerId, { attributes: ["name", "email"] })
+                return { ...e, buyer }
+            } 
+        ))
+        return {
+          data:{ count: data.length,
+            poster,
+            event,
+            tickcets: data}
+        }
+    }
+        catch(e){
+            return null
+        }
+
+    }))
+    bigData = bigData.filter((e)=>e!=null)
+    console.log("big data has been fetch")
+    return bigData
+
+}
+
+module.exports = { getAllTickets, getMyTickets, getEventTickets, createTicket, getATicket, ticketPostAndriod, getALLEventsTickets }
--- Routes/Tickets.js
+++ Routes/Tickets.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {getAllTickets,getMyTickets,getEventTickets,createTicket,getATicket,ticketPostAndriod} = require('../Controller/Tickets')
+const {getAllTickets,getMyTickets,getEventTickets,createTicket,getATicket,ticketPostAndriod, getALLEventsTickets} = require('../Controller/Tickets')
 
 
 //http://localhost:5000/tickets/all
@@ -8,6 +8,7 @@ const {getAllTickets,getMyTickets,getEventTickets,createTicket,getATicket,ticket
 router.get('/',getAllTickets)
 router.get('/user/:id',getMyTickets)
 router.get('/event/:id',getEventTickets)
+router.get('/event',getALLEventsTickets)
 router.get('/gen',createTicket)
 router.get('/:id',getATicket)
 router.post('/',ticketPostAndriod)
